030368c5492b7721196dd9ca3ead30e1df8bad0f
Benjamin Louis <louisbenetval@orange.fr>
2023-06-03 00:33:04 +0200

    MAJ

--- cinema.sql
+++ cinema.sql
@@ -41,20 +41,20 @@ LIMIT 1                                                                         
 --INFORMATIONS--(--Pour Peupler la Base de données--)
 
 INSERT INTO film (id_film, titre, annee_sortie, duree, synopsis, note, affiche, id_realisateur) VALUES
-(1, 'Star Wars Vol1', 1999, 133, 'Dans une galaxie une entre les Chevaliers Jedi et les Seigneurs noirs des Sith, personnes sensibles à la Force.', 6.2, 'film1.jpg', 1),
+(1, 'Star Wars Vol1', 1999, 136, 'Dans une galaxie une entre les Chevaliers Jedi et les Seigneurs noirs des Sith, personnes sensibles à la Force.', 6.2, 'film1.jpg', 1),
 (2, 'Indiana Jones Vol1', 1981, 116, 'Pérou, 1936. Le professeur Henry Walton Jones Jr (dit « Indiana », ou « Indy »), un éminent archéologue américain et expert en occultisme, est sur le point de mettre la main sur une idole Chachapoyan.', 7.7, 'film2.jpg', 2),
-(3, 'Batman Vol1', 1981, 206, 'Enfant, le milliardaire Bruce Wayne voit ses parents assassinés par un voleur des rues, qui en voulait au collier de perles de sa mère.', 7.1, 'film3.jpg', 3),
-(4, 'X-Men Vol1', 2000, 105, '1944, dans un camp de concentration. Séparé par la force de ses parents, le jeune Erik Magnus Lehnsherr se découvre ses pouvoirs sous le coup de la colère : il peut contrôler les métaux.', 6.5, 'film4.jpg', 4),
-(5, 'Grand Torino', 2008, 156, 'Walt Kowalski, vétéran de la guerre de Corée, raciste et irascible, vient de perdre sa femme. Une nuit, il surprend Thao, un de ses jeunes voisins Hmong, en train de voler sa Ford Gran Torino de 1972.', 7.7, 'film5.jpg', 5),
-(6, 'Mad Max', 1979, 133, 'Un criminel, Montazano, membre du gang de motards « Aigles de la route » , fuit en tuant un agent de la police routière.', 6.8, 'film6.jpg', 6),
-(7, 'Légende dautomne', 1994, 220, 'Dans le Montana dans les années 1960, un vieil indien raconte les secrets de la famille Ludlow.', 8.3, 'film7.jpg', 7),
-(8, 'Braveheart', 1995, 257, 'Au XIIIe siècle, le roi Edouard Ier sempare du trône dEcosse et instaure un régime répressif.', 8, 'film8.jpg', 8),
-(9, 'Star Wars Vol4', 1977, 201, 'Dans une galaxie une guerre entre les Chevaliers Jedi et les Seigneurs noirs des Sith, personnes sensibles à la Force.', 7.7, 'film9.jpg', 1),
-(10, 'La liste de Schindler', 1933, 315, 'En 1939, les Allemands installés en Pologne, regroupent les juifs dans des ghettos dans le but de les utiliser comme des esclaves. ', 8.1, 'film10.jpg', 2),
-(11, 'Dumbo', 2019, 112, '1919, dans le Missouri. Holt Farrier, ancien cavalier de cirque, mancho, revient au cirque Medici Brothers, dirigé par Max Medici, après la Première Guerre mondiale. ', 5.8, 'film11.jpg', 3),
-(12, 'Bullet Train', 2022, 206, 'Coccinelle est un assassin malchanceux et particulièrement déterminé à accomplir sa nouvelle mission paisiblement après que la plupart des ses missions aient déraillé.', 6.5, 'film12.jpg', 9),
-(13, 'Once Upon a time...', 2019, 241,'En 1969, la star de télévision Rick Dalton et le cascadeur Cliff Booth, sa doublure de longue date, poursuivent leurs carrières au sein d’une industrie qui a changé. ', 7.2, 'film13.jpg', 10),
-(14, 'Inglourious Basterds', 2009, 233, 'Dans la France occupée de 1940, Shosanna Dreyfus assiste à la mort de sa famille tombée entre les mains du colonel nazi Hans Landa.', 7.4, 'film14.jpg', 10);
+(3, 'Batman Vol1', 1981, 121, 'Enfant, le milliardaire Bruce Wayne voit ses parents assassinés par un voleur des rues, qui en voulait au collier de perles de sa mère.', 7.1, 'film3.jpg', 3),
+(4, 'X-Men Vol1', 2000, 104, '1944, dans un camp de concentration. Séparé par la force de ses parents, le jeune Erik Magnus Lehnsherr se découvre ses pouvoirs sous le coup de la colère : il peut contrôler les métaux.', 6.5, 'film4.jpg', 4),
+(5, 'Grand Torino', 2008, 116, 'Walt Kowalski, vétéran de la guerre de Corée, raciste et irascible, vient de perdre sa femme. Une nuit, il surprend Thao, un de ses jeunes voisins Hmong, en train de voler sa Ford Gran Torino de 1972.', 7.7, 'film5.jpg', 5),
+(6, 'Mad Max', 1979, 85, 'Un criminel, Montazano, membre du gang de motards « Aigles de la route » , fuit en tuant un agent de la police routière.', 6.8, 'film6.jpg', 6),
+(7, 'Légende dautomne', 1994, 133, 'Dans le Montana dans les années 1960, un vieil indien raconte les secrets de la famille Ludlow.', 8.3, 'film7.jpg', 7),
+(8, 'Braveheart', 1995, 178, 'Au XIIIe siècle, le roi Edouard Ier sempare du trône dEcosse et instaure un régime répressif.', 8, 'film8.jpg', 8),
+(9, 'Star Wars Vol4', 1977, 121, 'Dans une galaxie une guerre entre les Chevaliers Jedi et les Seigneurs noirs des Sith, personnes sensibles à la Force.', 7.7, 'film9.jpg', 1),
+(10, 'La liste de Schindler', 1933, 195, 'En 1939, les Allemands installés en Pologne, regroupent les juifs dans des ghettos dans le but de les utiliser comme des esclaves. ', 8.1, 'film10.jpg', 2),
+(11, 'Dumbo', 2019, 72, '1919, dans le Missouri. Holt Farrier, ancien cavalier de cirque, mancho, revient au cirque Medici Brothers, dirigé par Max Medici, après la Première Guerre mondiale. ', 5.8, 'film11.jpg', 3),
+(12, 'Bullet Train', 2022, 126, 'Coccinelle est un assassin malchanceux et particulièrement déterminé à accomplir sa nouvelle mission paisiblement après que la plupart des ses missions aient déraillé.', 6.5, 'film12.jpg', 9),
+(13, 'Once Upon a time...', 2019, 181,'En 1969, la star de télévision Rick Dalton et le cascadeur Cliff Booth, sa doublure de longue date, poursuivent leurs carrières au sein d’une industrie qui a changé. ', 7.2, 'film13.jpg', 10),
+(14, 'Inglourious Basterds', 2009, 153, 'Dans la France occupée de 1940, Shosanna Dreyfus assiste à la mort de sa famille tombée entre les mains du colonel nazi Hans Landa.', 7.4, 'film14.jpg', 10);
 
 --//////////////////////////////////////////////////////////////////////////////////////--
 
